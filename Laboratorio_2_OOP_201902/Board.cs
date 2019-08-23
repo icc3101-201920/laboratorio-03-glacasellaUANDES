899ec90bf893366f7ec7b0a83b191fad2c96a032
Gianfranco <glacasella@miuandes.cl>
2019-08-23 09:53:34 -0400

    Casi listo

--- Laboratorio_2_OOP_201902/Board.cs
+++ Laboratorio_2_OOP_201902/Board.cs
@@ -11,67 +11,15 @@ namespace Laboratorio_2_OOP_201902
         private const int DEFAULT_NUMBER_OF_PLAYERS = 2;
 
         //Atributos
-        private List<CombatCard>[] meleeCards;
-        private List<CombatCard>[] rangeCards;
-        private List<CombatCard>[] longRangeCards;
+        
 
-        private SpecialCard[] specialMeleeCards;
-        private SpecialCard[] specialRangeCards;
-        private SpecialCard[] specialLongRangeCards;
-        private SpecialCard[] captainCards;
+        private Dictionary<string, List<Card>>[] playerCards;
 
         private List<SpecialCard> weatherCards;
 
         //Propiedades
-        public List<CombatCard>[] MeleeCards
-        {
-            get
-            {
-                return this.meleeCards;
-            }
-        }
-        public List<CombatCard>[] RangeCards
-        {
-            get
-            {
-                return this.rangeCards;
-            }
-        }
-        public List<CombatCard>[] LongRangeCards
-        {
-            get
-            {
-                return this.longRangeCards;
-            }
-        }
-        public SpecialCard[] SpecialMeleeCards
-        {
-            get
-            {
-                return this.specialMeleeCards;
-            }
-        }
-        public SpecialCard[] SpecialRangeCards
-        {
-            get
-            {
-                return this.specialRangeCards;
-            }
-        }
-        public SpecialCard[] SpecialLongRangeCards
-        {
-            get
-            {
-                return this.specialLongRangeCards;
-            }
-        }
-        public SpecialCard[] CaptainCards
-        {
-            get
-            {
-                return this.captainCards;
-            }
-        }
+        public Dictionary<string, List<Card>>[] PlayerCards { get => this.playerCards; set => this.playerCards = value; }
+
         public List<SpecialCard> WeatherCards
         {
             get
@@ -84,134 +32,76 @@ namespace Laboratorio_2_OOP_201902
         //Constructor
         public Board()
         {
-            this.meleeCards = new List<CombatCard>[DEFAULT_NUMBER_OF_PLAYERS];
-            this.rangeCards = new List<CombatCard>[DEFAULT_NUMBER_OF_PLAYERS];
-            this.longRangeCards = new List<CombatCard>[DEFAULT_NUMBER_OF_PLAYERS];
-            this.specialMeleeCards = new SpecialCard[DEFAULT_NUMBER_OF_PLAYERS];
-            this.specialRangeCards = new SpecialCard[DEFAULT_NUMBER_OF_PLAYERS];
-            this.specialLongRangeCards = new SpecialCard[DEFAULT_NUMBER_OF_PLAYERS];
-            this.captainCards = new SpecialCard[DEFAULT_NUMBER_OF_PLAYERS];
             this.weatherCards = new List<SpecialCard>();
+            this.playerCards = new Dictionary<string, List<Card>>[
+            DEFAULT_NUMBER_OF_PLAYERS]; //Inicializa el arreglo de diccionarios.
+            this.playerCards[0] = new Dictionary<string, List<Card>>(); // Inicializa los diccionarios.
+            this.playerCards[1] = new Dictionary<string, List<Card>>(); // Inicializa los diccionarios.
         }
-
 
 
         //Metodos
-        public void AddCombatCard(int playerId, CombatCard combatCard)
+        public void AddCard(Card card, int playerId = -1, string buffType = null)
         {
-            if (combatCard.Type == "melee")
+            //Revisar si la carta recibida en el parmetro es Combat o Special
+            if (card.GetType().Name == nameof(CombatCard))
             {
-                meleeCards[playerId].Add(combatCard);
-            }
-            else if (combatCard.Type == "range")
-            {
-                rangeCards[playerId].Add(combatCard);
-            }
-            else
-            {
-                longRangeCards[playerId].Add(combatCard);
-            }
-            
-        }
-        public void AddSpecialCard(SpecialCard specialCard, int playerId = -1, string buffType = null)
-        {
-            if (specialCard.Type == "captain")
-            {
+
+                //En caso de que sea de combate , agregarla al diccionario del jugador correspondiente , revisando el parmetro playerId
                 if (playerId == 0 || playerId == 1)
                 {
-                    captainCards[playerId] = specialCard;
-                }
-                else
-                {
-                    throw new IndexOutOfRangeException();
-                }
-            }
-            else if (specialCard.Type == "weather")
-            {
-                weatherCards.Add(specialCard);
-            }
-            else
-            {
-                if (buffType != null)
-                {
-                    if (playerId == 0 || playerId == 1)
+                    //Se revisa si el diccionario ya tiene alguna carta del tipo que se quiere agregar, por ejemplo melee.
+                    //Constainskey revisa si existe la llave en el diccionario.En caso de existir retorna true. Por ejemlo, si la carta es de tipo "melee",
+                    //el mtodo buscar si existe la llave "melee" en el diccionario.
+                    if (playerCards[playerId].ContainsKey(card.Type))
                     {
-                        if (buffType == "melee")
-                        {
-                            specialMeleeCards[playerId] = specialCard;
-                        }
-                        else if (buffType == "range")
-                        {
-                            specialRangeCards[playerId] = specialCard;
-                        }
-                        else
-                        {
-                            specialLongRangeCards[playerId] = specialCard;
-                        }
+                        //Si es que existe no es necesario crear la lista cartas, por lo que se agrega directamente
+                        playerCards[playerId][card.Type].Add(card);
                     }
                     else
                     {
-                        throw new IndexOutOfRangeException();
-                    }   
+                        //Si no existe , se agrega un nuevo par "Llave": Valor al diccionario.
+                        //Este par tiene como "Llave" el tipo de la carta , ejemplo "melee".
+                        //Este par tiene como valor la lista de cartas , con la primera carta que se va a agregar.
+                        playerCards[playerId].Add(card.Type , new List<Card>() { card });
+                    }
                 }
                 else
                 {
-                    throw new ArgumentNullException();
+                    throw new IndexOutOfRangeException("No player id given");
                 }
             }
-        }
-        public void DestroyCombatCards()
-        {
-            this.meleeCards = new List<CombatCard>[DEFAULT_NUMBER_OF_PLAYERS];
-            this.rangeCards = new List<CombatCard>[DEFAULT_NUMBER_OF_PLAYERS];
-            this.longRangeCards = new List<CombatCard>[DEFAULT_NUMBER_OF_PLAYERS];
-        }
-        public void DestroySpecialCards()
-        {
-            this.specialMeleeCards = new SpecialCard[DEFAULT_NUMBER_OF_PLAYERS];
-            this.specialRangeCards = new SpecialCard[DEFAULT_NUMBER_OF_PLAYERS];
-            this.specialLongRangeCards = new SpecialCard[DEFAULT_NUMBER_OF_PLAYERS];
-            this.weatherCards = new List<SpecialCard>();
-        }
-        public int[] GetMeleeAttackPoints()
-        {
-            //Debe sumar todos los puntos de ataque de las cartas melee y retornar los valores por jugador.
-            int[] totalAttack = new int[] { 0, 0 };
-            for (int i=0; i < 2; i++)
+            else
             {
-                foreach (CombatCard meleeCard in meleeCards[i])
+                // Asumimos que la carta es Special, por el else
+                // Revisamos si la carta es captain
+                if (card.GetType().Name == "captain")
                 {
-                    totalAttack[i] += meleeCard.AttackPoints;
+                    // Revisamos si el playerId es valido
+                    if (playerId == 0 || playerId == 1)
+                    {
+                        // Revisamos si el diccionario ya contiene el key. Como dice el enunciado,
+                        // si contiene el key no dejamos que agregue la carta
+                        if (playerCards[playerId].ContainsKey("captain"))
+                        {
+                            throw new Exception("El jugador ya tiene una carta captain");
+                        }
+                        // Si no, le agregamos el capitan. De esta forma el capitan se agrega una sola vez durante el juego
+                        else
+                        {
+                            playerCards[playerId].Add("captain", new List<Card>() { card });
+                        }
+                    }
                 }
+                
+                 
+                
+                //Es buffer? IMPLEMENTAR
+                //Revisar si no se a agregado un buffer en la fila de tipo de combate del diccionario que representa las cartas del jugador, en caso de existir, no permitir que se agregue la carta.
+                //Para esto , asuma que los valores de buffType pueden ser "melee", "range" o "longRange". Luego , asuma que la llave de esta carta ser card.Type + buffType , por ejemplo,
+                //en el caso de que exista un buff en range , la llave sera Buffrange. Debe revisar que no exista la llave card.Type+buffType.
+                //Es weather? Agregue la carta weather a la lista de cartas weather.
             }
-            return totalAttack;
-            
-        }
-        public int[] GetRangeAttackPoints()
-        {
-            //Debe sumar todos los puntos de ataque de las cartas range y retornar los valores por jugador.
-            int[] totalAttack = new int[] { 0, 0 };
-            for (int i = 0; i < 2; i++)
-            {
-                foreach (CombatCard rangeCard in rangeCards[i])
-                {
-                    totalAttack[i] += rangeCard.AttackPoints;
-                }
-            }
-            return totalAttack;
-        }
-        public int[] GetLongRangeAttackPoints()
-        {
-            //Debe sumar todos los puntos de ataque de las cartas longRange y retornar los valores por jugador.
-            int[] totalAttack = new int[] { 0, 0 };
-            for (int i = 0; i < 2; i++)
-            {
-                foreach (CombatCard longRangeCard in longRangeCards[i])
-                {
-                    totalAttack[i] += longRangeCard.AttackPoints;
-                }
-            }
-            return totalAttack;
         }
     }
 }
--- Laboratorio_2_OOP_201902/Deck.cs
+++ Laboratorio_2_OOP_201902/Deck.cs
@@ -16,24 +16,21 @@ namespace Laboratorio_2_OOP_201902
 
         public List<Card> Cards { get => this.cards; set => this.cards = value; }
 
-        // Como ahora la lista acepta cartas de tipo Card, no es
-        // necesario tener dos addCard, podemos ahorrar codigo
+        // para el addCard y el DestroyCard no hace falta tener 4 metodos. Como 
+        // la lista acepta tipo card, podemos usar 2 y listo!!! Ahorramos codigo
         public void AddCard(Card card)
         {
             this.cards.Add(card);
-            return;
         }
-
-        // Tampoco hace falta tener dos Destroy, sino uno solo con el cardId
-        public void DestroyCard(int cardId)
+        public void DestroyCard(int CardId)
         {
-            cards.RemoveAt(cardId);
-            return;
+            this.cards.RemoveAt(CardId);
         }
-
+        
         public void Shuffle()
         { 
             throw new NotImplementedException();
         }
+
     }
 }
